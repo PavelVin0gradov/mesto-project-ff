0ced6cc8c55a520dc2ced23dfdbc97f9427e811b
Pasha Pasha <baber1231@yandex.ru>
2024-07-15 01:01:10 +0300

    add cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector("#card-template").content;
+
 // @todo: DOM узлы
 
-// @todo: Функция создания карточки
+const cardContainer = document.querySelector(".places__list");
 
 // @todo: Функция удаления карточки
 
+function delCard(evt) {
+  evt.target.closest(".card").remove();
+}
+
+// @todo: Функция создания карточки
+
+function addCard(img, title) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const buttonDelCard = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").src = img;
+  cardElement.querySelector(".card__title").textContent = title;
+
+  buttonDelCard.addEventListener("click", delCard);
+
+  cardContainer.append(cardElement);
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardData) => {
+  const cardImg = cardData.link;
+  const cardTitle = cardData.name;
+
+  addCard(cardImg, cardTitle);
+});
